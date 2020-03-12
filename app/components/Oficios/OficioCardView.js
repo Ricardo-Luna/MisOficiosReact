48afd45a1d6b817d0accae157cdcb382a2226e58
rickylunat@hotmail.com <rickylunat@hotmail.com>
2020-03-12 15:00:31 -0600

    Modulized cardview

--- app/components/Oficios/OficioCardView.js
+++ app/components/Oficios/OficioCardView.js
@@ -1,48 +1,109 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { Image, Icon } from "react-native-elements";
+import React, { useEffect, useState } from "react";
+
+import { View, Text, StyleSheet } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import Moment from "moment";
 
 export default function OficiosCardView() {
+  const [docs, setDocs] = useState([]);
+  const [actualizar, setActualizar] = useState([]);
+  const [loading, setloading] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "http://10.0.0.17/ApiMisOficios/api/Documentos/Buscar?offset=0&limit=5&idcarpeta=88C8C04F-29B7-487D-B9E4-62D0E9433FD4",
+      {
+        method: "GET"
+      }
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        setDocs(responseJson["Documentos"]);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  const dateFormater = dt => {
+    Moment.locale("en");
+    return <Text>{Moment(dt).format("d/MM/YYYY  HH:mm")}</Text>; //basically you can do all sorts of the formatting and others
+  };
   return (
-    <View style={styles.layout}>
-      <Icon
-        style={styles.iconImportance}
-        name="archive-arrow-down-outline"
-        type="material-community"
-        color="black"
-        reverseColor="black"
-      />
-      <Icon
-        style={styles.iconImportance}
-        name="alert-circle-outline"
-        type="material-community"
-        color="cyan"
-        reverseColor="black"
-      />
-      <Text> </Text>
-      <Text style={styles.folio}>FE/002/2019</Text>
-      <Text> </Text>
-      <Text>06/12/2019</Text>
-      <Text> </Text>
-      <Text>09:56</Text>
-      <Text> </Text>
-      <Icon
-        style={styles.iconImportance}
-        name="attach-file"
-        type="material"
-        color="black"
-        reverseColor="black"
-      />
-      <Text> </Text>
-      <Icon
-        style={styles.iconImportance}
-        name="arrow-down-bold-box-outline"
-        type="material-community"
-        color="black"
-        reverseColor="black"
-      />
-      <Text> </Text>
-      <Text style={styles.content}>...</Text>
+    <View>
+      {docs.map((u, i) => {
+        return (
+          <Card key={i}>
+            {/*Padre del componente */}
+            <View
+              accessibilityRole='button'
+              onPress={console.log("Silenced")
+              }
+              style={{ flexDirection: "column" }}
+            >
+              {/*Elementos header */}
+              <View style={styles.iconsinicio}>
+                {u.Importancia < 3 ? (
+                  <Icon
+                    name={"alert-circle-outline"}
+                    type="material-community"
+                    color={u.Importancia === 2 ? "cyan" : "grey"}
+                    reverseColor="black"
+                  />
+                ) : (
+                  <Icon
+                    name={"alert-outline"}
+                    type="material-community"
+                    color="red"
+                    reverseColor="black"
+                  />
+                )}
+                <Text> </Text>
+                <Text>{u.Codigo}</Text>
+                <Text> </Text>
+
+                <Text> {dateFormater(u.FechaCreacion)}</Text>
+                <Text> </Text>
+                <View style={styles.iconsfin}>
+                  {u.TieneArchivosAdjuntos && (
+                    <Icon
+                      name="attach-file"
+                      type="material"
+                      color="black"
+                      reverseColor="black"
+                    />
+                  )}
+
+                  <Icon
+                    name="arrow-down-bold-box-outline"
+                    type="material-community"
+                    color="black"
+                    reverseColor="black"
+                  />
+                  <Icon
+                    name="eye-check"
+                    type="material-community"
+                    color="black"
+                    reverseColor="black"
+                  />
+                </View>
+              </View>
+              <View>
+                <Text style={styles.asunto}>{u.Asunto}</Text>
+              </View>
+              <View style={styles.footerCard}>
+                <Icon
+                  name="send"
+                  type="material-community"
+                  color="black"
+                  reverseColor="black"
+                />
+                <Text> </Text>
+                <Text style={styles.destinatarios}> {u.Destinatarios} </Text>
+              </View>
+            </View>
+          </Card>
+        );
+      })}
     </View>
     /* <CardView cardElevation={2} cardMaxElevation={2} cornerRadius={5}>
       <Text>Elevation 0</Text>
@@ -50,29 +111,30 @@ export default function OficiosCardView() {
   );
 }
 const styles = StyleSheet.create({
-  layout: {
-    backgroundColor: "powderblue",
-    flexDirection: "row"
-  },
-  iconImportance: {
-    alignSelf: "stretch",
+  iconsinicio: {
     flexDirection: "row",
-    position: "absolute"
+    flex: 1,
+    top: -4
   },
-  folio: {
-    justifyContent: 'center',
-    alignItems:'center', 
+  iconsfin: {
+    flexDirection: "row",
+    flex: 1,
+    top: 0,
+    justifyContent: "flex-end"
   },
-  files: {
-    alignSelf: "stretch"
+  footerCard: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignContent: "flex-start"
   },
-  recieved: {
-    alignSelf: "flex-end"
+  pantalla: {
+    backgroundColor: "#000000"
   },
-  seen: {
-    alignSelf: "flex-end"
+  asunto: {
+    fontSize: 16
   },
-  content: {
-    flexDirection: "column"
+  destinatarios: {
+    fontSize: 10,
+    top: 5
   }
 });
--- app/screens/Oficios.js
+++ app/screens/Oficios.js
@@ -1,149 +1,26 @@
 import React, { Component, useEffect, useState } from "react";
 
-import { View, Text, Image, StyleSheet, Flatlist } from "react-native";
-import { Card, ListItem, Button, Icon } from "react-native-elements";
+import { View, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import BottomSheet from "reanimated-bottom-sheet";
+import OficiosCardView from "../components/Oficios/OficioCardView";
 import Carpetas from "../components/Carpetas/Carpetas";
 import Header from "../components/Carpetas/Header";
-import Moment from "moment";
 
 export default function Oficios() {
-  const [docs, setDocs] = useState([]);
-  const [actualizar, setActualizar] = useState([]);
-  useEffect(() => {
-    fetch(
-      "http://10.0.0.17/ApiMisOficios/api/Documentos/Buscar?offset=0&limit=50&idcarpeta=A65465FD-6A5A-49B3-95DD-3FC5BB3A71F5",
-      {
-        method: "GET"
-      }
-    )
-      .then(response => response.json())
-      .then(responseJson => {
-        setDocs(responseJson["Documentos"]);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
-  const dateFormater = dt => {
-    Moment.locale("en");
-    return <Text>{Moment(dt).format("d MMM")}</Text>; //basically you can do all sorts of the formatting and others
+  const renderHeader = () => {
+    <Header />;
   };
-  const renderHeader = () =>{
-    <Header/>
-  }
   return (
     <ScrollView>
-      {docs.map((u, i) => {
-        return (
-          <Card 
-          key={i}
-          >
-            {/*Padre del componente */}
-            <View style={{ flexDirection: "column" }}>
-              {/*Elementos header */}
-              <View style={styles.iconsinicio}>
-                {u.Importancia < 3 ? (
-                  <Icon
-                    name={"alert-circle-outline"}
-                    type="material-community"
-                    color={u.Importancia === 2 ? "cyan" : "grey"}
-                    reverseColor="black"
-                  />
-                ) : (
-                  <Icon
-                    name={"alert-outline"}
-                    type="material-community"
-                    color="red"
-                    reverseColor="black"
-                  />
-                )}
-                <Text> </Text>
-                <Text>{u.Codigo}</Text>
-                <Text> </Text>
-                <Text>{u.FechaEnvio}</Text>
-                <Text> </Text>
-                <Text> {dateFormater(u.FechaEnvio)}</Text>
-                <Text> </Text>
-                <View style={styles.iconsfin}>
-                  {u.TieneArchivosAdjuntos && (
-                    <Icon
-                      name="attach-file"
-                      type="material"
-                      color="black"
-                      reverseColor="black"
-                    />
-                  )}
+      <OficiosCardView />
 
-                  <Icon
-                    name="arrow-down-bold-box-outline"
-                    type="material-community"
-                    color="black"
-                    reverseColor="black"
-                  />
-                  <Icon
-                    name="eye-check"
-                    type="material-community"
-                    color="black"
-                    reverseColor="black"
-                  />
-                </View>
-              </View>
-              <View>
-                <Text style={styles.asunto}>{u.Asunto}</Text>
-              </View>
-              <View style={styles.footerCard}>
-                <Icon
-                  name="send"
-                  type="material-community"
-                  color="black"
-                  reverseColor="black"
-                />
-                <Text> </Text>
-                <Text style={styles.destinatarios}> {u.Destinatarios} </Text>
-              </View>
-            </View>
-          </Card>
-        );
-      })}
       <View>
-        <BottomSheet 
-        snapPoints={[300, 200, 0]} 
-
-        renderHeader={renderHeader()}
-        />
+        <BottomSheet snapPoints={[300, 200, 0]} renderHeader={renderHeader()} />
+        {bottomSheetRef.current.snapTo(10)}
       </View>
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  iconsinicio: {
-    flexDirection: "row",
-    flex: 1,
-    top: -4
-  },
-  iconsfin: {
-    flexDirection: "row",
-    flex: 1,
-    top: 0,
-    justifyContent: "flex-end"
-  },
-  footerCard: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignContent: "flex-start"
-  },
-  pantalla: {
-    backgroundColor: "#000000"
-  },
-  asunto: {
-    fontSize: 16
-  },
-  destinatarios: {
-    fontSize: 10,
-    top: 5
-  }
-});
+const styles = StyleSheet.create({});
