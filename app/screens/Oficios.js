40f1d55976c1d6268e9cb1d568b68769859d8858
rickylunat@hotmail.com <rickylunat@hotmail.com>
2020-03-17 14:57:17 -0600

    Action Button display fixed

--- app/screens/Oficios.js
+++ app/screens/Oficios.js
@@ -3,11 +3,11 @@ import { Icon } from "react-native-elements";
 import { View, StyleSheet, Button } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import ActionButton from "react-native-action-button";
+import { Actions } from "react-native-router-flux";
 import RBSheet from "react-native-raw-bottom-sheet";
 import OficiosCardView from "../components/Oficios/OficioCardView";
 import Carpetas from "../components/Carpetas/Carpetas";
 import Loading from "../components/Loading";
-import { Actions } from "react-native-router-flux";
 
 export default function Oficios(props) {
   const [carpetas, setCarpetas] = useState(
@@ -17,11 +17,43 @@ export default function Oficios(props) {
   const refRBSheet = useRef();
   const { setCarpetaActual } = props;
   return (
-    <View>
-      <OficiosCardView carpeta={carpetas} setLoading={setLoading} />
-      <ActionButton buttonColor="#46babc" offsetY={10} offsetX={10}>
+    <View style={styles.buttonsheet}>
+      <View style={styles.cardview}>
+        <OficiosCardView carpeta={carpetas} setLoading={setLoading} />
+        <RBSheet
+          style={styles.buttonsheet}
+          ref={refRBSheet}
+          height={300}
+          duration={600}
+          closeOnDragDown={true}
+          closeOnPressMask={true}
+          customStyles={{
+            wrapper: {
+              backgroundColor: "transparent"
+            },
+            draggableIcon: {
+              backgroundColor: "grey"
+            }
+          }}
+        >
+          <Carpetas
+            setCarpetaID={setCarpetas}
+            setCarpetaActual={setCarpetaActual}
+          />
+        </RBSheet>
+        <Loading text="Cargando Archivos" isVisible={loading} />
+      </View>
+      <ActionButton
+        buttonColor="#46babc"
+        buttonText="☰"
+        degrees={90}
+        offsetY={10}
+        offsetX={10}
+        position={"right"}
+      >
         <ActionButton.Item
           buttonColor="#00b0e1"
+          title="Carpetas"
           onPress={() => {
             refRBSheet.current.open();
           }}
@@ -33,7 +65,7 @@ export default function Oficios(props) {
             color="black"
           />
         </ActionButton.Item>
-        <ActionButton.Item buttonColor="#6fb74d">
+        <ActionButton.Item title="Ayuda" buttonColor="#6fb74d">
           <Icon
             name={"help"}
             backgroundColor="transparent"
@@ -41,7 +73,7 @@ export default function Oficios(props) {
             color="black"
           />
         </ActionButton.Item>
-        <ActionButton.Item buttonColor="#d52f89">
+        <ActionButton.Item title="Buscar" buttonColor="#d52f89">
           <Icon
             name={"magnify"}
             backgroundColor="transparent"
@@ -49,46 +81,31 @@ export default function Oficios(props) {
             color="black"
           />
         </ActionButton.Item>
-        <ActionButton.Item buttonColor="#f08119">
+        <ActionButton.Item title="Cerrar sesión" buttonColor="#f08119">
           <Icon
             name={"account-arrow-left"}
             backgroundColor="transparent"
             type="material-community"
             color="black"
+            onPress={() => {
+              Actions.splash();
+            }}
           />
         </ActionButton.Item>
       </ActionButton>
-
-      <RBSheet
-        ref={refRBSheet}
-        height={300}
-        duration={500}
-        closeOnDragDown={true}
-        closeOnPressMask={true}
-        customStyles={{
-          wrapper: {
-            backgroundColor: "transparent"
-          },
-          draggableIcon: {
-            backgroundColor: "grey"
-          }
-        }}
-      >
-        <Carpetas
-          setCarpetaID={setCarpetas}
-          setCarpetaActual={setCarpetaActual}
-        />
-      </RBSheet>
-      <Loading text="Cargando Archivos" isVisible={loading} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   buttonsheet: {
-    flex: 1,
-    justifyContent: "flex-end"
+    flex: 2,
+    flexDirection: "row"
+
     //backgroundColor: "transparent"
+  },
+  cardview:{
+    flex: 1
   },
   viewScreen: {
     flex: 1,
